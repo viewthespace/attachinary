b83796a35975fdc1be5055d13de7065c1273bd04
Karl Baum <karl.baum@gmail.com>
2016-04-15 17:02:00 -0400

    added filename to permitted attributes

--- lib/attachinary/utils.rb
+++ lib/attachinary/utils.rb
@@ -15,7 +15,7 @@ module Attachinary
                  Attachinary::File.new hash.slice(*Attachinary::File.attr_accessible[:default].to_a)
                else
                  hash.symbolize_keys!
-                 permitted_params = ActionController::Parameters.new(hash.slice(:public_id, :version, :width, :height, :format, :resource_type)).permit!
+                 permitted_params = ActionController::Parameters.new(hash.slice(:public_id, :version, :width, :height, :format, :resource_type, :filename)).permit!
                  Attachinary::File.new(permitted_params)
                end
         file.scope = scope.to_s if scope && file.respond_to?(:scope=)
